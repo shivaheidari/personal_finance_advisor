77a44220ac6459b2a7b58dc0a64438acb688e894
Shiva Heydari <shivaheydari.es@gmail.com>
2025-10-20 15:36:13 -0600

    router_port

--- app/api/routes/portfolio.py
+++ app/api/routes/portfolio.py
@@ -1,29 +1,29 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import APIRouter, HTTPException
 from services.portfolio.portfolio import PortfolioService
 
-app = FastAPI()
+router = APIRouter()
 portfolio_service = PortfolioService()
 
-@app.post("/portfolio/")
+@router.post("/")
 def create_portfolio(portfolio: dict):
     result = portfolio_service.create_portfolio(portfolio)
     return result
 
-@app.get("/portfolio/{user_id}")
+@router.get("/{user_id}")
 def get_portfolio(user_id: str):
     result = portfolio_service.get_portfolio(user_id)
     if not result:
         raise HTTPException(status_code=404, detail="Portfolio not found")
     return result
 
-@app.put("/portfolio/{portfolio_id}")
+@router.put("/{portfolio_id}")
 def update_portfolio(portfolio_id: str, user_id: str, new_data: dict):
     result = portfolio_service.update_portfolio(portfolio_id, user_id, new_data)
     if not result:
         raise HTTPException(status_code=404, detail="Portfolio not found")
     return result
 
-@app.delete("/portfolio/{portfolio_id}")
+@router.delete("/{portfolio_id}")
 def delete_portfolio(portfolio_id: str, user_id: str):
     result = portfolio_service.delete_portfolio(portfolio_id, user_id)
     if not result:
