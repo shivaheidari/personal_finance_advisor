af806ed750978a163ab88aa5c7e1cfdeae4f7975
Shiva Heydari <shivaheydari.es@gmail.com>
2025-10-21 15:56:55 -0600

    Main class

---
+++ services/advisor/advisor.py
@@ -0,0 +1,15 @@
+
+"""
+Advisor service (Main class)
+get_recommendation(user_id)
+get_risk_profile(user_id)
+train_model()
+
+cordinates data fetches from 
+1)portfolio
+2)MarketDataService
+
+orchestrtes rule-based engine and/or AI model calls
+
+"""
+
